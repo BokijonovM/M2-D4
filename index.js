8a0bba52823f10676353a36d4633e190b6848341
Bokijonov Mukhsinjon <78298971+BokijonovM@users.noreply.github.com>
2021-11-11 15:53:37 +0100

    user input part is done

---
+++ index.js
@@ -0,0 +1,37 @@
+const removeButtons = function(){
+    let removeButtons = document.getElementById("button-addon1")
+    let removeElem = document.getElementById("remove-button")
+    let byr = removeElem.style.display="none"
+    removeButtons.onclick = byr
+}
+
+const removeButtons1 = function(){
+    let removeButtons = document.getElementById("button-addon2")
+    let removeElem = document.getElementById("remove-button1")
+    let byr = removeElem.style.display="none"
+    removeButtons.onclick = byr
+}
+const removeButtons2 = function(){
+    let removeButtons = document.getElementById("button-addon3")
+    let removeElem = document.getElementById("remove-button2")
+    let byr = removeElem.style.display="none"
+    removeButtons.onclick = byr
+}
+const removeButtons3 = function(){
+    let removeButtons = document.getElementById("button-addon4")
+    let removeElem = document.getElementById("remove-button3")
+    let byr = removeElem.style.display="none"
+    removeButtons.onclick = byr
+}
+const removeButtons4 = function(){
+    let removeButtons = document.getElementById("button-addon5")
+    let removeElem = document.getElementById("remove-button4")
+    let byr = removeElem.style.display="none"
+    removeButtons.onclick = byr
+}
+const removeButtons5 = function(){
+    let removeButtons = document.getElementById("button-addon6")
+    let removeElem = document.getElementById("remove-button5")
+    let byr = removeElem.style.display="none"
+    removeButtons.onclick = byr
+}
